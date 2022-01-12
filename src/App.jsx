762b2f36cb54ac52710e875e0ea47dba64989e62
tamsay <longetemitope@gmail.com>
2022-01-12 01:56:23 +0100

    implementation of multi input fields with dynamic text inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import makeAnimated from 'react-select/animated';
 import axios from 'axios'
 import AsyncSelect from 'react-select/async';
@@ -7,8 +7,22 @@ import AsyncSelect from 'react-select/async';
 function App() {
   const [inputValue, setValue] = useState('');
   const [selectedValue, setSelectedValue] = useState(null);
+  
+  const [testArray, setTestArray] = useState([]);
 
   const animatedComponents = makeAnimated();
+
+  useEffect(() =>{
+    const loadOptions = async () => {
+      const res = await axios.get('http://jsonplaceholder.typicode.com/posts') // replace this with your api endpoint
+      const data = res.data
+      let modifiedData = data.map(data => {
+        return {...data, tags : []}
+      })
+      setTestArray(modifiedData)
+  };
+  loadOptions()
+  },[])
   
 // handle input change event
 const handleInputChange = value => {
@@ -20,13 +34,23 @@ const handleChange = value => {
   setSelectedValue(value);
 }
 
+const addNewTag =(e, index)=>{
+  if(e.key.toLowerCase() === 'enter'){
+      let value = e.target.value
+      let newTestArray = [...testArray];
+      newTestArray[index].tags.push(value);
+      setTestArray(newTestArray)
+      e.target.value = ""
+  }
+}
+
 const loadOptions = async (inputValue) => {
     const res = await axios.get('http://jsonplaceholder.typicode.com/posts') // replace this with your api endpoint
     const data = res.data
     let filteredData = data.filter((i) => i.title.toLowerCase().includes(inputValue.toLowerCase()))  
     return filteredData;
-
 };
+
   return (
     <div className="App">
         <h3>React Select Project</h3>
@@ -44,6 +68,19 @@ const loadOptions = async (inputValue) => {
         components={animatedComponents}
         isMulti
       />
+      </div>
+      <div>
+        {testArray.map((element, index)=>{
+          return(
+            <div>
+              <input type="text" onKeyPress={(e)=>addNewTag(e, index)} />
+              <p>{element.title}</p>
+              <ul className="tagWrapper">
+                {element.tags.map((tag)=> (<li>{tag}</li>))}
+              </ul>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
